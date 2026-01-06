51cc9915c5920d3c44ea1d0c271b51a29c5b62ca
GibranSaputra11 <gibranbantul@gmail.com>
2026-01-06 11:55:28 +0700

    add logika like dan dislike

--- app/routes/index.js
+++ app/routes/index.js
@@ -37,10 +37,33 @@ router.post("/send", async (req, res) => {
   }
 });
 
+
 // TODO: Tambahkan Route LIKE di sini
 // Clue: router.post('/like/:id', async (req, res) => { ... })
+router.post('/like/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        // Query menambah nilai likes sebanyak +1 [cite: 197]
+        await db.query('UPDATE menfess SET likes = likes + 1 WHERE id = ?', [id]);
+        res.redirect('/'); // Redirect kembali ke halaman utama [cite: 200]
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Gagal mengupdate Like");
+    }
+});
 
 // TODO: Tambahkan Route DISLIKE di sini
 // Clue: Mirip like, tapi yang ditambah kolom dislikes
+router.post('/dislike/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        // Query menambah nilai dislikes sebanyak +1 [cite: 198]
+        await db.query('UPDATE menfess SET dislikes = dislikes + 1 WHERE id = ?', [id]);
+        res.redirect('/'); // Redirect kembali ke halaman utama [cite: 200]
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Gagal mengupdate Dislike");
+    }
+});
 
 module.exports = router;
